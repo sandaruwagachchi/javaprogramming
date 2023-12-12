496160332b5df35d09a8ebc935fbf81dfffd00af
sandaruwagachchi <wagachchisandaru@gmail.com>
2023-12-12 20:08:29 +0530

    Print hello java

---
+++ MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass
+  {
+    public static void sample()
+    {
+       System.out.println("Hello");
+    }
+public static void main (String[] args)
+    {
+      MyClass.sample();
+    }
+  }  
