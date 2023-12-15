392b6a646249e79b07e2ba2256473d5fa6b5c114
sandaruwagachchi <wagachchisandaru@gmail.com>
2023-12-15 21:31:35 +0530

    Non static member

---
+++ MyClass2.java
@@ -0,0 +1,12 @@
+public class MyClass2
+  {
+    public void sample()
+    {
+       System.out.println("Hello");
+    }
+public static void main (String[] args)
+    {
+      MyClass2 myclass2=new MyClass2();
+      myclass2.sample();
+    }
+  }  
