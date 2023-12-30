959fa3b1f8c7f7bb21922d9cb9c7e42f8141fd31
sandaruwagachchi <wagachchisandaru@gmail.com>
2023-12-30 10:08:45 +0530

    practiceDefaultConstructer

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+public static void main(String[] args)
+{
+Child child1=new Child();
+}
+}
---
+++ Parent.java
@@ -0,0 +1,7 @@
+public class Parent
+{
+public Parent()
+{
+System.out.println("This is a default constructor of parents class");
+}
+}
