a5c3f1f566eab9d5daaaaf5853205ce795f6a433
sandaruwagachchi <wagachchisandaru@gmail.com>
2023-12-31 10:56:11 +0530

    singleInheritance

---
+++ SingleInheritence/Child.java
@@ -0,0 +1,7 @@
+public class Child extends Parent
+{
+public void childMethod()
+{
+System.out.println("You are welcome!");
+}
+}
---
+++ SingleInheritence/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+public static void main(String[] args)
+{
+Child child=new Child();
+child.parentMethod();
+child.childMethod();
+}
+}
